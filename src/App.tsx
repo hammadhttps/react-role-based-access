1a9033e4bbbdb82b56076fd073bb5ae3ea999976
Hammad-ul-Hassan <103960660+HammadulHassan5812@users.noreply.github.com>
2025-07-24 19:07:30 +0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { BrowserRouter as Router,Route,Routes,Navigate } from 'react-router-dom'
+import LoginPage from './pages/loginPage'
+import UnauthorizedPage from './pages/UnauthorizedPage'
+import ProtectedRoute from './context/ProtectedRoute'
+import AdminPage from './pages/AdminPage'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        <Route path='/login' element={ <LoginPage/> }/>
+        <Route path='./unauthorized' element={<UnauthorizedPage/>} />
+        <Route path='/admin' element={<ProtectedRoute roles={['admin']}>
+          <AdminPage/>
+
+        </ProtectedRoute>}></Route>
+
+        <Route path='*' element={<Navigate to='/login'/>}/>
+      </Routes>
+    </Router>
+
   )
 }
 
